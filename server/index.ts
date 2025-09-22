2306888f787489aba988c6dea8570479c7021b0a
Bean Jackson <jaggedbytes@protonmail.com>
2025-09-22 14:59:43 -0400

    fix: enable local dev

--- server/index.ts
+++ server/index.ts
@@ -63,8 +63,8 @@ app.use((req, res, next) => {
   const port = parseInt(process.env.PORT || '5000', 10);
   server.listen({
     port,
-    host: "0.0.0.0",
-    reusePort: true,
+    host: process.env.NODE_ENV === "production" ? "0.0.0.0" : "localhost",
+    reusePort: process.env.NODE_ENV === "production",
   }, () => {
     log(`serving on port ${port}`);
   });
